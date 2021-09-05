091a49233697b144195172983c03691924389c6a
snrk0622 <snrk.0622@gmail.com>
2021-09-05 15:43:27 +0900

    feat: react-navigationをinstallしてTimelineScreenを登録

--- react_native/App.jsx
+++ react_native/App.jsx
@@ -1,22 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import TimelineScreen from './src/screens/TimelineScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Timeline" component={TimelineScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ react_native/src/screens/TimelineScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TimelineScreen = () => {
+  return (
+    <View>
+      <Text>TimelineScreen</Text>
+    </View>
+  );
+};
+
+export default TimelineScreen;
