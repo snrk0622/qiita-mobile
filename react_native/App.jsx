7e4e81a7ddfa9eafc5295fe22dee7fec6545b8e0
snrk0622 <snrk.0622@gmail.com>
2021-09-06 09:17:42 +0900

    feat: SearchScreenを追加

--- react_native/App.jsx
+++ react_native/App.jsx
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 import TimelineScreen from './src/screens/TimelineScreen';
+import SearchScreen from './src/screens/SearchScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -31,6 +32,21 @@ export default function App() {
             tabBarIcon: (focused) => {
               const color = focused ? '#55C500' : 'rgba(51, 51, 51, 0.1)';
               return <FontAwesome5 name="clock" size={24} color={color} />;
+            },
+            tabBarLabel: (focused) => {
+              const color = focused ? '#55C500' : 'rgba(51, 51, 51, 0.1)';
+              return <Text style={{ color, fontSize: 10, fontWeight: 'bold' }}>タイムライン</Text>;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            headerShown: false,
+            tabBarIcon: (focused) => {
+              const color = focused ? '#55C500' : 'rgba(51, 51, 51, 0.1)';
+              return <FontAwesome5 name="search" size={24} color={color} />;
             },
             tabBarLabel: (focused) => {
               const color = focused ? '#55C500' : 'rgba(51, 51, 51, 0.1)';
